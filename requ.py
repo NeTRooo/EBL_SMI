951e892863bae6dc7156494976f8b856af41117a
linum1337 <lebishova.elena@mail.ru>
2023-03-14 13:26:58 +0300

    Добавил внесение пикчи и тега в бд

--- requ.py
+++ requ.py
@@ -1,6 +1,7 @@
 import requests
 import json
-
+import os
+from PIL import Image
 
 def load():
     import requests
@@ -15,12 +16,30 @@ def load():
     return response.json()
 
 def get_image(tag):
-    img = load()['_items']
-    for i in range(len(img)):
-        crate = img[i]['tag']
-        if crate == tag:
-            res = img[i]['img']
+    try:
+        img = load()['_items']
+        for i in range(len(img)):
+            crate = img[i]['tag']
+            if crate == tag:
+                res = img[i]['img']
 
-    return res
+        img_data = requests.get(res).content
+        print(img_data)
+        with open('image_name.jpg', 'wb') as handler:
+            handler.write(img_data)
+    except:
+        return 'get_image func failed'
 
-print(get_image('nigger'))
+
+
+def upload_image(img, tag):
+    try:
+        url = 'http://127.0.0.1:5000/image'
+        payload = json.dumps({'img': img, 'tag': tag})
+        headers = {
+            'Content-Type': 'application/json',
+        }
+
+        response = requests.post(url, headers=headers, data=payload)
+    except:
+        return 'upload_image func failed'
