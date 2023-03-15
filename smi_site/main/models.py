1d059aad63fdcfdd975407d644bf1696ac047df3
DenSSS <dimon.yablonovskiyy@mail.ru>
2023-03-15 22:01:21 +0300

    Тобавил что-то установите библиотеку                                                     pip install django-taggit

--- smi_site/main/models.py
+++ smi_site/main/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 class Upload(models.Model):
     upload_file = models.FileField()    
     upload_date = models.DateTimeField(auto_now_add =True)
 
 class ContentLinks(models.Model):
-    upload = models.OneToOneField(Upload, on_delete=models.CASCADE, verbose_name='Лики к контенту')
+    upload = models.OneToOneField(Upload, on_delete=models.CASCADE, verbose_name='Линки к контенту')
     link = models.TextField(verbose_name='Ссылка на контент')
+    tags = TaggableManager()
     
     class Meta:
         verbose_name = 'Ссылка на контент'
