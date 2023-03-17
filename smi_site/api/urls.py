d5b175b74d3778bb656e26625da15ca27b300704
DenSSS <dimon.yablonovskiyy@mail.ru>
2023-03-17 11:26:29 +0300

    Добавил API поиск по тегу

--- smi_site/api/urls.py
+++ smi_site/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('v1/', views.UploadListApiView.as_view(), name='api_v1'),
+    path('v1/all/', views.UploadListApiView.as_view(), name='api_all'),
+    path('v1/tag/', views.UploadListCreateAPIView.as_view(), name='api_tag'),
 ]
--- smi_site/api/views.py
+++ smi_site/api/views.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView
 from main.models import Upload
 from .serializers import UploadSerializer
+from rest_framework import filters
  
 class UploadListApiView(ListAPIView):
     queryset = Upload.objects.all()
     serializer_class = UploadSerializer
 
+class UploadListCreateAPIView(ListCreateAPIView):
+    search_fields = ['tags__name']
+    filter_backends = (filters.SearchFilter,)
+    queryset = Upload.objects.all()
+    serializer_class = UploadSerializer
