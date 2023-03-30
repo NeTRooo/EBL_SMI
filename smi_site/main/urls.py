4f1436aa3039679ef60086c7ca9a776dcee81661
DenSSS <dimon.yablonovskiyy@mail.ru>
2023-03-30 11:37:44 +0300

    Ну вродь, первая версия закончена

--- smi_site/main/urls.py
+++ smi_site/main/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     # path('', views.main_page, name='main_page'),
     path('', views.test, name='test'),
-    re_path('^search/(?P<search>.+)/$', views.SearchPageFilter, name='SearchPageFilter'),
+    re_path('^search/(?P<search>.+)/$', views.SearchPageFilter, name='search_filter_page'),
     path('search/', views.SearchPage, name='search_page'),
     path('upload/', views.UploadPage, name='upload_page'),
     # path('upload/', views.UploadView.as_view(), name='fileupload'),
